9d3e59ef707014bc650427d613c1e1bf1fa9dd8f
avanim <axm1205@case.edu>
2023-06-09 20:06:36 -0400

    created Vote model and seed data

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -14,7 +15,11 @@ class Post(Base):
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
 
     user = relationship('User')
-    # Deleting post from DB deletes all its comments
+    # Deleting post from DB deletes all its comments and votes
     comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
 
-
+    # Dynamic property to add up votes associated to this post
+    vote_count = column_property(
+        select([func.count(Vote.id)]).where(Vote.post_id == id)
+    )
