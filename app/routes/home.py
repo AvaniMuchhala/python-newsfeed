d90512b6a3bd07137f67794ef871ff7a991e9587
avanim <axm1205@case.edu>
2023-06-14 17:58:56 -0400

    render homepage with posts info

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,15 +1,23 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
+# At /, render homepage with posts data
 @bp.route('/')
 def index():
-  return render_template('homepage.html')
+  # Get all posts in descending order of creation time
+  db = get_db()
+  posts = db.query(Post).order_by(Post.created_at.desc()).all()
+  return render_template('homepage.html', posts=posts)
 
+# At /login, render login
 @bp.route('/login')
 def login():
   return render_template('login.html')
 
+# At /post/<id>, render single post with that ID
 @bp.route('/post/<id>')
 def single(id):
   return render_template('single-post.html')
